3fab1faa954d66bb32f429e19185c30c8c4f5c22
Akhil Mohan <akhilerm@gmail.com>
2016-09-17 11:55:53 +0530

    Game screen set

--- tictoc.py
+++ tictoc.py
@@ -1,4 +1,26 @@
 import pygame
 
-Game_Matrix = [[0 for x in range(9)] for y in range(9)]
-print Game_Matrix
+SCREENWIDTH = SCREENHEIGHT = 512
+
+#check whether a matrix is owned by someone
+def check(matrix):
+    print "Test"
+
+
+#The game matrix initialized with None, False for O and True for X, Posession_Matrix stores which all submatrices is possessed by O and X
+Game_Matrix = [[None for x in range(9)] for y in range(9)]
+Posession_MatrixO = [None for x in range(3) for y in range (3)]
+Posession_MatrixX = [None for x in range(3) for y in range (3)]
+
+
+def main():
+    pygame.init()
+    SCREEN = pygame.display.set_mode((SCREENWIDTH,SCREENHEIGHT))
+    pygame.display.set_caption('Tic Toc')
+    #main game loop
+    while True:
+           print 'hi'
+
+
+if __name__ == '__main__':
+        main()
